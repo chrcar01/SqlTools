d14d5233de424419d841e5dcf810147b1a0b8ad3
Chris Carter <chrisjcarter@gmail.com>
2019-03-21 06:32:43 -0600

    adding AssemblyInfo to .NET Framework projects

---
+++ SqlTools.Oracle/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SqlTools.Oracle")]
+[assembly: AssemblyDescription("SqlTools - thin wrapper over ADO.NET")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ChrisJohn Rocks")]
+[assembly: AssemblyProduct("SqlTools")]
+[assembly: AssemblyCopyright("Copyright © 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("3ad830ee-1f44-4abd-b96a-0ea749b7dd29")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
